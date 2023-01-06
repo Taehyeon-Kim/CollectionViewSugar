411ac9e9cc17e710094253f54e570fa08ac8d4ca
김태현 <taehyk528@gmail.com>
2023-01-06 13:07:37 +0900

    Init package

---
+++ Sources/CollectionViewSugar/CollectionViewSugar.swift
@@ -0,0 +1,6 @@
+public struct CollectionViewSugar {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CollectionViewSugarTests/CollectionViewSugarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CollectionViewSugar
+
+final class CollectionViewSugarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CollectionViewSugar().text, "Hello, World!")
+    }
+}
